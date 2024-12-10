df20ec1faf078a533095633f4eb902294c0732f4
ademecheklie <ademecheklie78@gmail.com>
2024-12-10 11:57:20 +0300

    fixing swagger

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,6 +1,7 @@
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import path from 'path';
+
 const options = {
   definition: {
     openapi: '3.0.0',
@@ -21,37 +22,32 @@ const options = {
     security: [{ bearerAuth: [] }],
     servers: [
       {
-        url: 'https://book-collection-api-one.vercel.app/',
+        url: 'https://book-collection-api-one.vercel.app/', 
         description: 'Development server',
       },
     ],
   },
-  apis: [path.resolve('src/routes/*.js')], // Update path to match your file structure
+  apis: ['src/routes/*.js'], 
 };
-
 
 const swaggerSpec = swaggerJsdoc(options);
 
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.1/swagger-ui.min.css";
+
 const swaggerDocs = (app) => {
-  // Route to serve Swagger JSON
   app.get('/swagger.json', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(swaggerSpec);
   });
 
-  // Route for Swagger UI
+  // Route to serve Swagger UI
   app.use(
     '/api-docs',
     swaggerUi.serve,
-    swaggerUi.setup(null, {
-      swaggerOptions: {
-        url: 'https://book-collection-epmy4hw09-ademecheklies-projects.vercel.app/swagger.json', // URL to your working Swagger JSON endpoint
-      },
+    swaggerUi.setup(swaggerSpec, {
+      customCssUrl: CSS_URL, 
     })
   );
-  
-  
-  
 
   console.log('Swagger docs available at /api-docs');
 };
